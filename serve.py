c1ef72f3fe9bc5e4be1a25d91fcd6796739ca104
“Xinyu <xinyulianuiuc@gmail.com>
2023-05-08 00:47:52 -0500

    init

---
+++ serve.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+import subprocess
+import socket
+import os
+
+app = Flask(__name__)
+
+@app.route('/',methods = ['POST'])
+def run_stress_cpu():
+   p = subprocess.Popen(["python3", stress_cpu.py], stdout=subprocess.PIPE)
+
+@app.route('/',methods = ['GET'])
+def get_seed():
+   hostname = socket.gethostname()
+   ip = socket.gethostbyname(hostname)
+   return ip
+
+if __name__ == '__main__':
+   app.run(host='0.0.0.0',debug = True)
+
